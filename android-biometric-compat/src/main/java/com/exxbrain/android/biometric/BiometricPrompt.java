96c4c4e6d27aefe885ca9306e42a5dcaf18a1ee0
Denis Zakharov <d.zakharov@bssys.com>
2019-10-09 19:08:39 +0300

    Build fixes

--- android-biometric-compat/src/main/java/com/exxbrain/android/biometric/BiometricPrompt.java
+++ android-biometric-compat/src/main/java/com/exxbrain/android/biometric/BiometricPrompt.java
@@ -26,7 +26,6 @@ import android.os.Handler;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RestrictTo;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
